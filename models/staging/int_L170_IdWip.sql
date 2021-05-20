84d8b60992677ffe37493220925a0e427338e50a
pouriaI5O <61250960+pouriaI5O@users.noreply.github.com>
2021-05-20 14:37:38 -0400

    Update int_L170_IdWip.sql

--- models/staging/int_L170_IdWip.sql
+++ models/staging/int_L170_IdWip.sql
@@ -3,7 +3,7 @@ cast(CONCAT(Date, ' '+CAST(hour AS VARCHAR(2))+':'+CAST(minute AS VARCHAR(2))) a
 cast(Timestamps AS time) as Time,
 Timestamps AT TIME ZONE 'UTC'  AS Utc_Timestamp
 from ( select Count(*) as Duration,Date,Hour,Minute,Id_Wip
-from {{ ref('stg_removedublicate170_times_id')}}
+from {{ ref('stg_removedublicate170_times_id')}}   
        group by Id_Wip,Date,Hour,Minute
        order by Date)
        where (Time >'06:00:00' and Time <'14:30:00')
