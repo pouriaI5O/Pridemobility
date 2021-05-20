7889c27c777398e57af2d91d330c9878e93d4cd5
pouriaI5O <61250960+pouriaI5O@users.noreply.github.com>
2021-05-20 16:00:29 -0400

    Update and rename fct_Line170_Status.sql to fct_line170_status.sql

--- models/core/fct_line170_status.sql
+++ models/core/fct_line170_status.sql
@@ -4,4 +4,4 @@ Status,Ole_Status,Utc_Timestamp,
      WHEN Time >'10:45:00' and Time <'11:25:00'THEN 0
      WHEN Time >'12:50:00' and Time <'13:05:00'THEN 0
      ELSE 1 END AS Break_Filter
- from {{ ref('int_Avg_Ole_Sta170')}} where Break_Filter>0
+ from {{ ref('int_avg_ole_sta170')}} where Break_Filter>0
