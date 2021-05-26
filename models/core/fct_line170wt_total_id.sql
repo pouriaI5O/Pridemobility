5932fef9e0872b73fc3505943f07c088888925bb
Pouria Modaresi <pouria@ifiveo.com>
2021-05-26 17:20:19 +0000

    Editing Utc_Timestamp column and changing timestamp column to Local_timestamp

--- models/core/fct_line170wt_total_id.sql
+++ models/core/fct_line170wt_total_id.sql
@@ -1,7 +1,6 @@
 select hour,minute,Date,Id_Wt as chair_id,Duration,
-cast(CONCAT(date, ' '+CAST(hour AS VARCHAR(2))+':'+CAST(minute AS VARCHAR(2))) as datetime)as Timestamps,
-Timestamps AT TIME ZONE 'UTC'  AS
-       Utc_Timestamp,
+cast(CONCAT(date, ' '+CAST(hour AS VARCHAR(2))+':'+CAST(minute AS VARCHAR(2))) as datetime)as Local_Timestamp,
+ DATEADD(HOUR, +4, Local_Timestamp) as  Utc_Timestamp,
  CASE WHEN Time>'08:10:00' and Time <'08:25:00'THEN 0
      WHEN Time >'10:45:00' and Time <'11:25:00'THEN 0
      WHEN Time >'12:50:00' and Time <'13:05:00'THEN 0
